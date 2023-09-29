c1012f83d0836f0726095116ad3f22924832029a
ChuHuy <ChuHuy743@gmail.com>
2023-09-29 07:00:43 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Program
+{
+    public static void main(string[] args)
+    {
+        int a, b;
+        //hien thi
+        Console.WriteLine("Nhap a=");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap b=");
+        b = int.Parse(Console.ReadLine());
+        int tong = a + b;
+        Console.WriteLine("tong a+b" + a + "+" + b + "=" + tong);
+        //in ra ket qua
+        Console.Read();
+    }
+}
